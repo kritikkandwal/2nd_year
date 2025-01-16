3b9412022017db6bd01844b3ef47616a4a71e64b
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-01-16 06:38:23 +0530

    Add files via upload

---
+++ pattern_12.java
@@ -0,0 +1,27 @@
+//         1
+//       2 1 2
+//     3 2 1 2 3
+//   4 3 2 1 2 3 4
+// 5 4 3 2 1 2 3 4 5
+
+
+import java.util.*;
+class pattern_12{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
