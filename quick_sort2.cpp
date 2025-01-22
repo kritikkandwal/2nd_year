5b59a154cfe45ebf40c73498b75ac1fb21a769eb
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-01-22 16:50:06 +0530

    Add files via upload

---
+++ quick_sort2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int start, int end) {
+    int pivot = arr[end];
+    int i = start - 1;
+
+    for (int j = start; j < end; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[end]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int start, int end) {
+    if (start < end) {
+        int pi = partition(arr, start, end);
+
+        quickSort(arr, start, pi - 1);
+        quickSort(arr, pi + 1, end);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
