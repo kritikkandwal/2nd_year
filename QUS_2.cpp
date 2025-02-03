b312ba9e8fca6b873ab935233e88a5f2a13442d2
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-02-03 22:58:22 +0530

    Add files via upload

---
+++ QUS_2.cpp
@@ -0,0 +1,4 @@
+// Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+#include<iostream>
+using namespace std;
+str
