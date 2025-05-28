f15c27a7f249ceb3eb0b2569208f7e060f1c0a24
kritik kandwal <149930398+kritikkandwal@users.noreply.github.com>
2025-05-28 13:35:24 +0530

    Add files via upload

---
+++ C3_conditional.py
@@ -0,0 +1,10 @@
+age=int(input("enter age: "))
+if(age>=18):{
+    print("bada ho gya ladka apka")
+}
+elif(age<=0):{
+    print("bacha to krlo phale")
+}
+else:{
+    print("sote padenge chote")
+}
